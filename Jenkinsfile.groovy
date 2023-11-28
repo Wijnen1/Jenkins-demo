b6d9a98b784505b41859fa5bcc1153e33f5c55e0
Wijnen1 <70098292+Wijnen1@users.noreply.github.com>
2023-11-28 12:30:26 +0100

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -8,24 +8,10 @@ pipeline {
     }
 
     stages {
-        stage('Create Jenkins Agent Pod') {
-            steps {
-                script {
-                    def jnlpPodName = 'jnlp-pod-name'
-                    def jnlpImage = 'jenkins/jnlp-slave:latest'
-                    
-                    // Create Jenkins Agent (JNLP) pod
-                    sh "kubectl run $jnlpPodName --image=$jnlpImage --restart=Always"
-                }
-            }
-        }
-
         stage('Create Nginx Pod') {
             steps {
                 script {
                     def nginxPodName = 'nginx-pod-name'
-                    
-                    // Create NGINX pod
                     sh "kubectl run $nginxPodName --image=nginx --restart=Always"
                 }
             }
@@ -37,17 +23,10 @@ pipeline {
                     def gitRepoUrl = 'https://github.com/Wijnen1/Jenkins-demo.git'
                     def gitRepoDir = 'github-repo'
                     def nginxPodName = 'nginx-pod-name'
-                    def jnlpPodName = 'jnlp-pod-name'
 
-                    // Clone the Git repository
                     sh "git clone $gitRepoUrl $gitRepoDir"
-
-                    // Copy HTML and CSS files to NGINX pod
                     sh "kubectl cp $gitRepoDir/index.html $nginxPodName:/usr/share/nginx/html/index.html"
                     sh "kubectl cp $gitRepoDir/style.css $nginxPodName:/usr/share/nginx/html/style.css"
-
-                    // Copy Git repository to Jenkins Agent (JNLP) pod
-                    sh "kubectl cp $gitRepoDir $jnlpPodName:/workspace/"
                 }
             }
         }
@@ -55,7 +34,7 @@ pipeline {
 
     post {
         success {
-            echo 'De pods en bestanden zijn succesvol aangemaakt en bijgewerkt.'
+            echo 'De pod en bestanden zijn succesvol aangemaakt en bijgewerkt.'
         }
         failure {
             echo 'Het proces is mislukt.'
