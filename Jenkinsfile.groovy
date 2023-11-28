fb491919c7c2eee510ffffe878d6b42c8cc30fb8
Wijnen1 <70098292+Wijnen1@users.noreply.github.com>
2023-11-28 12:40:56 +0100

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,43 +1,20 @@
-pipeline {
-    agent {
-        label 'kubeagent'
-    }
+podTemplate(containers: [
+    containerTemplate(
+        name: 'jnlp', 
+        image: 'jenkins/inbound-agent:latest'
+        )
+  ]) {
 
-    environment {
-        KUBECONFIG = credentials('kubernetes-credentials')
-    }
-
-    stages {
-        stage('Create Nginx Pod') {
-            steps {
-                script {
-                    def nginxPodName = 'nginx-pod-name'
-                    sh "kubectl run $nginxPodName --image=nginx --restart=Always"
+    node(POD_LABEL) {
+        stage('Get a Maven project') {
+            container('jnlp') {
+                stage('Shell Execution') {
+                    sh '''
+                    echo "Hello! I am executing shell"
+                    '''
                 }
             }
         }
 
-        stage('Download HTML and CSS') {
-            steps {
-                script {
-                    def gitRepoUrl = 'https://github.com/Wijnen1/Jenkins-demo.git'
-                    def gitRepoDir = 'github-repo'
-                    def nginxPodName = 'nginx-pod-name'
-
-                    sh "git clone $gitRepoUrl $gitRepoDir"
-                    sh "kubectl cp $gitRepoDir/index.html $nginxPodName:/usr/share/nginx/html/index.html"
-                    sh "kubectl cp $gitRepoDir/style.css $nginxPodName:/usr/share/nginx/html/style.css"
-                }
-            }
-        }
-    }
-
-    post {
-        success {
-            echo 'De pod en bestanden zijn succesvol aangemaakt en bijgewerkt.'
-        }
-        failure {
-            echo 'Het proces is mislukt.'
-        }
     }
 }
